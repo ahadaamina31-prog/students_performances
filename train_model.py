c5692b3d0e6f60aa903376d6c094dc13e70d4ee9
LCH <ahadaamina31@gmail.com>
2026-06-04 11:38:16 +0530

    aaaa

--- train_model.py
+++ train_model.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score
 import joblib
 
 # Load dataset
-data = pd.read_excel("Student_Performence(1).xlsx")
+data = pd.read_excel("Student_Performence.xlsx")
 
 # Create target column
 data["pass_fail"] = (data["math score"] >= 50).astype(int)
